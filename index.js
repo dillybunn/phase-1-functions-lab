8c23584d4400ee17dcbe805206bec540f71f6803
Billy Dunn <bdunn27@gmail.com>
2024-03-11 14:51:40 -0400

    phase 1 function lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - 42);
+}
+
+const distanceFromHqInFeet = (street) => {
+  const blocks = distanceFromHqInBlocks(street);
+  return blocks * 264;
+};
+
+const distanceTravelledInFeet = (start, destination) => {
+  const blocks = Math.abs(destination - start);
+  return blocks * 264;
+};
+const calculatesFarePrice = (start, destination) => {
+  const feet = distanceTravelledInFeet(start, destination);
+
+  if (feet < 400) {
+    return 0;
+  }
+
+  if (feet < 2000) {
+    return (feet - 400) * 0.02;
+  }
+
+  if (feet < 2500) {
+    return 25;
+  }
+  return "cannot travel that far";
+};
